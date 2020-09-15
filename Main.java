38995d0354a209f18d70604c6b2ac656cc45944c
IzzyEhnes <izzyehnes@gmail.com>
2020-09-15 14:07:43 -0700

    Feature: Add LList class

--- Main.java
+++ Main.java
@@ -7,6 +7,19 @@ public class Main
         private Node next;
 
 
+        public Node()
+        {
+        }
+
+
+
+        public Node(int inValue)
+        {
+            this.value = inValue;
+        }
+
+
+
         int getValue()
         {
             return value;
@@ -31,6 +44,65 @@ public class Main
         void setNext(Node inNode)
         {
             next = inNode;
+        }
+    }
+
+
+
+    public class LList
+    {
+        Node head;
+
+        void add(int inValue)
+        {
+            Node nn = new Node(inValue);
+
+            nn.setNext(head);
+
+            head = nn;
+        }
+
+
+
+        void traverse()
+        {
+            Node current = new Node();
+
+            for (current = head; current != null; current = current.getNext())
+            {
+                System.out.println(current.getValue());
+            }
+        }
+
+
+
+        void delete(int inValue)
+        {
+            Node current = new Node();
+
+            if (head != null)
+            {
+                if (head.value == inValue)
+                {
+                    head = head.next;
+                }
+
+                else
+                {
+                    current = head;
+
+                    while (current.next != null &&
+                            current.next.value != inValue)
+                    {
+                        current = current.next;
+                    }
+
+                    if (current.next != null)
+                    {
+                        current.next = current.next.next;
+                    }
+                }
+            }
         }
     }
 
