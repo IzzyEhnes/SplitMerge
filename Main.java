d41810eae27eb573cf99ae69bd34fd8a60d44bab
IzzyEhnes <izzyehnes@gmail.com>
2020-09-15 13:48:54 -0700

    File: Add Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public class Node
+    {
+        private int value;
+
+        private Node next;
+    }
+
+    public static void main(String[] args)
+    {
+	// write your code here
+    }
+}
