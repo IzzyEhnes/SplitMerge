39999c0c8096e8dface2b8d1892b06146fa0dd9b
IzzyEhnes <izzyehnes@gmail.com>
2020-09-15 13:53:03 -0700

    Feature: Add Node class

--- Main.java
+++ Main.java
@@ -5,7 +5,36 @@ public class Main
         private int value;
 
         private Node next;
+
+
+        int getValue()
+        {
+            return value;
+        }
+
+
+
+        void setValue(int inValue)
+        {
+            this.value = inValue;
+        }
+
+
+
+        Node getNext()
+        {
+            return next;
+        }
+
+
+
+        void setNext(Node inNode)
+        {
+            next = inNode;
+        }
     }
+
+
 
     public static void main(String[] args)
     {
